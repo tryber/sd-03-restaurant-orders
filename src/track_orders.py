1b0d57ae92e0cab005ac0b6f3ccea454027b2f66
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-13 19:16:50 -0300

    requisito 1: track_orders.py

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,40 @@
+from analyze_log import (
+    get_most_requested_recipe,
+    get_qnt_the_recipe_was_ordered,
+    get_recipes_never_ordered,
+    get_unvisited_days
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return get_most_requested_recipe(self.orders, costumer)
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return get_qnt_the_recipe_was_ordered(self.orders, costumer, order)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return get_recipes_never_ordered(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return get_unvisited_days(self.orders, costumer)
+
+
+if __name__ == "__main__":
+    tracker = TrackOrders()
+    print(tracker.__len__())
+    tracker.add_new_order("ana", "pizza", "domingo")
+    tracker.add_new_order("maria", "sopa", "segunda-feira")
+    print(tracker.get_most_ordered_dish_per_costumer("ana"))
+    print(tracker.get_order_frequency_per_costumer("ana", "pizza"))
+    print(tracker.get_never_ordered_per_costumer("ana"))
+    print(tracker.get_days_never_visited_per_costumer("ana"))
