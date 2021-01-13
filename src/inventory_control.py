0c14b56032420b6e4bd8b06be28bc95627a4050e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-13 19:54:23 -0300

    requisito 3: inventory_control.py - validações 1 a 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,24 +1,47 @@
 class InventoryControl:
     def __init__(self):
         self.ingredients = {
-            'hamburguer': ['pao', 'carne', 'queijo'],
-            'pizza': ['massa', 'queijo', 'molho'],
-            'misto-quente': ['pao', 'queijo', 'presunto'],
-            'coxinha': ['massa', 'frango'],
+            "hamburguer": ["pao", "carne", "queijo"],
+            "pizza": ["massa", "queijo", "molho"],
+            "misto-quente": ["pao", "queijo", "presunto"],
+            "coxinha": ["massa", "frango"],
         }
 
         self.minimum_inventory = {
-            'pao': 50,
-            'carne': 50,
-            'queijo': 100,
-            'molho': 50,
-            'presunto': 50,
-            'massa': 50,
-            'frango': 50,
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
         }
 
-    def add_new_order(self, costumer, order, day):
-        pass
+        self.inventory = {
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
+        }
+
+    def add_new_order(self, costumer, order, _day):
+        order_ingredients = self.ingredients[order]
+        for ingredient in order_ingredients:
+            self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        ingredients_to_buy = {}
+        for ingredient, qnt in self.inventory.items():
+            ingredients_to_buy[ingredient] = (
+                self.minimum_inventory[ingredient] - self.inventory[ingredient]
+            )
+        return ingredients_to_buy
+
+
+if __name__ == "__main__":
+    inventory = InventoryControl()
+    inventory.add_new_order("", "pizza", "")
+    print(inventory.get_quantities_to_buy())
