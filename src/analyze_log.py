8080e1a1b389ae12282aa2bc625613083954a502
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-13 19:20:53 -0300

    lendo o arquivo de log e criando um dicionario

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,18 @@
-# Primeiro commit - #vqv
+import csv
+
 
 def analyse_log(path_to_file):
-    raise NotImplementedError
+    data = {}
+    if not path_to_file.endswith('.csv'):
+        raise ValueError('Arquivo inválido')
+    with open(path_to_file) as csv_file:
+        csv_dict = csv.reader(csv_file, delimiter=",")
+        for name, dish, day in csv_dict:
+            if name not in data:
+                data[name] = [(dish, day)]
+            else:
+                data[name].append((dish, day))
+    print(data)
+
+
+analyse_log('../data/orders_1.csv')
