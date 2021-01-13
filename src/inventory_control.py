284de2cd65d68d7b4d7bdaec3f21df2208190182
Gustaff <gustavohmfigueiredo@gmail.com>
2021-01-13 20:08:22 -0300

    send one more time

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -8,30 +8,35 @@ class InventoryControl:
         }
 
         self.minimum_inventory = {
-            'pao': 50,
-            'carne': 50,
-            'queijo': 100,
-            'molho': 50,
-            'presunto': 50,
-            'massa': 50,
-            'frango': 50,
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
         }
 
         self.inventory = {
-            'pao': 50,
-            'carne': 50,
-            'queijo': 100,
-            'molho': 50,
-            'presunto': 50,
-            'massa': 50,
-            'frango': 50,
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
         }
 
     def add_new_order(self, costumer, order, day):
+        ingrs = []
         for ingredient in self.ingredients[order]:
-            self.inventory[ingredient] -= 1
+            if self.inventory[ingredient] <= 0:
+                return False
+            ingrs.append(ingredient)
+        for ingr in ingrs:
+            self.inventory[ingr] -= 1
 
-    def get_shopping_list(self):
+    def get_quantities_to_buy(self):
         return {
             ingredient: self.minimum_inventory[ingredient] - qnt
             for ingredient, qnt in self.inventory.items()
@@ -44,22 +49,16 @@ class InventoryControl:
             for ingredient in ingredients:
                 if self.inventory[ingredient] == 0:
                     available_dishes.pop()
+                    break
 
         return available_dishes
 
 
 if __name__ == "__main__":
-    inventory = InventoryControl()
-    inventory.add_new_order("jorge", "hamburguer", "terça-feira")
-    inventory.add_new_order("jorge", "hamburguer", "terça-feira")
-    hamburguer = inventory.get_shopping_list()
-    total_ingredients = {
-        "pao": 2,
-        "carne": 2,
-        "queijo": 2,
-        "molho": 0,
-        "presunto": 0,
-        "massa": 0,
-        "frango": 0,
-    }
-    print(hamburguer)
+    ingredients = InventoryControl()
+    count = 1
+    while count <= 50:
+        ingredients.add_new_order("jorge", "coxinha", "terça-feira")
+        count += 1
+    dishes = ingredients.get_available_dishes()
+    print(dishes)
--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def print_info(tracker, control):
     print(tracker.get_order_frequency_per_costumer("arnaldo", "hamburguer"))
     print(tracker.get_never_ordered_per_costumer("joao"))
     print(tracker.get_days_never_visited_per_costumer("joao"))
-    print(control.get_shopping_list())
+    print(control.get_quantities_to_buy())
 
 
 def main():
