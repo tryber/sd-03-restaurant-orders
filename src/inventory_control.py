b7b41b8d4c3c301ee2751f4881270831027100cc
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-13 20:18:30 -0300

    requisito 4 - todas as validações

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -30,6 +30,8 @@ class InventoryControl:
     def add_new_order(self, _costumer, order, _day):
         order_ingredients = self.ingredients[order]
         for ingredient in order_ingredients:
+            if self.inventory[ingredient] <= 0:
+                return False
             self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
