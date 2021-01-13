d58680728d0a3bee69b2d43446eed633cc7e3a14
Gustaff <gustavohmfigueiredo@gmail.com>
2021-01-13 17:22:07 -0300

    cahnging main

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ from track_orders import TrackOrders
 
 
 def print_info(tracker, control):
-    print(tracker.get_most_ordered_dish_per_costumer('maria'))
-    print(tracker.get_order_frequency_per_costumer('arnaldo', 'hamburguer'))
-    print(tracker.get_never_ordered_per_costumer('joao'))
-    print(tracker.get_days_never_visited_per_costumer('joao'))
+    print(tracker.get_most_ordered_dish_per_costumer("maria"))
+    print(tracker.get_order_frequency_per_costumer("arnaldo", "hamburguer"))
+    print(tracker.get_never_ordered_per_costumer("joao"))
+    print(tracker.get_days_never_visited_per_costumer("joao"))
     print(control.get_quantities_to_buy())
 
 
 def main():
-    topic = 'order'
+    topic = "order"
     path = ""
 
     tracker = TrackOrders()
@@ -24,9 +24,10 @@ def main():
         pub.subscribe(sub, topic)
 
     with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for costumer, order, day in csv_reader:
-            pub.sendMessage(topic, costumer=costumer, order=order, day=day)
+            if order in control.get_available_dishes():
+                pub.sendMessage(topic, costumer=costumer, order=order, day=day)
 
     print_info(tracker, control)
 
