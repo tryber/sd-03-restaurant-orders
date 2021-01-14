229d3333cde155b5cb81a222b0f8432d33c20208
tales <talestales@gmail.com>
2021-01-14 10:12:15 -0300

    a

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,77 @@
+import csv
+
+
+def check_path_and_format(file_name, file_ext):
+    if not file_name.endswith(file_ext):
+        print("Formato inválido")
+        return True
+    return False
+
+
+def most_requested_food(data):
+    dict_resp = {}
+    most_frequent = data['Orders'][0]
+    for values in data['Orders']:
+        if(values not in dict_resp):
+            dict_resp[values] = 1
+        else:
+            dict_resp[values] += 1
+        if(dict_resp[values] > dict_resp[most_frequent]):
+            most_frequent = values
+    return most_frequent
+
+
+def most_type_food(food, data):
+    count_food = 0
+    for values in data:
+        if(values == food):
+            count_food += 1
+    return count_food
+
+
+def never_requested_meal(all_meals, data):
+    meals = set(data)
+    return all_meals.difference(meals)
+
+
+def days_that_wasnt_in_place(all_days, data):
+    cur_days = set(data)
+    return all_days.difference(cur_days)
+
+
+def import_csv(path_to_file):
+    if(check_path_and_format(path_to_file, '.csv')):
+        return True
+    all_days = set()
+    all_meals = set()
+    data = ''
+    resp = {}
+    with open(path_to_file) as file:
+        data = csv.reader(file, delimiter=",")
+        for customer, order, day in data:
+            if(customer not in resp):
+                resp[customer] = {
+                    'Orders': [],
+                    'Days': [],
+                }
+            resp[customer]['Orders'].append(order)
+            resp[customer]['Days'].append(day)
+            all_days.add(day)
+            all_meals.add(order)
+    return resp, all_days, all_meals
+
+
 def analyse_log(path_to_file):
-    raise NotImplementedError
+    resp, all_days, all_meals = import_csv(path_to_file)
+    if(resp is True):
+        return True
+    with open('output.txt', 'w+') as file:
+        file.write(f"{most_requested_food(resp['maria'])}\n")
+        file.write(
+            f"{most_type_food('hamburguer', resp['arnaldo']['Orders'])}\n")
+        file.write(
+            f"{never_requested_meal(all_meals, resp['joao']['Orders'])}\n"
+            )
+        file.write(
+            f"{days_that_wasnt_in_place(all_days, resp['joao']['Days'])}"
+            )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,61 @@
+from src.analyse_log import (
+    most_requested_food,
+    most_type_food,
+    never_requested_meal,
+    days_that_wasnt_in_place
+)
+
+
 class TrackOrders:
-    def __len__(self):
-        pass
+
+    def __init__(self):
+        self.dict_orders = {}
+        self.days = set()
+        self.count_days = {}
+        self.orders = set()
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if(costumer not in self.dict_orders):
+            self.dict_orders[costumer] = {
+                'Orders': [],
+                'Days': [],
+            }
+        self.count_days[day] = self.count_days.get(day, 0) + 1
+        self.dict_orders[costumer]['Orders'].append(order)
+        self.dict_orders[costumer]['Days'].append(day)
+        self.days.add(day)
+        self.orders.add(order)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return most_requested_food(self.dict_orders[costumer])
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return most_type_food(order, self.dict_orders[costumer]['Orders'])
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_requested_meal(
+            self.orders, self.dict_orders[costumer]['Orders']
+        )
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return days_that_wasnt_in_place(
+            self.days, self.dict_orders[costumer]['Days']
+        )
 
     def get_busiest_day(self):
-        pass
+        max_tuple = max(self.count_days.items(), key=lambda x: x[1])
+        busiest_days = [
+            day
+            for day, count in self.count_days.items()
+            if count == max_tuple[1]
+        ]
+        return busiest_days
 
     def get_least_busy_day(self):
-        pass
+        min_tuple = min(self.count_days.items(), key=lambda x: x[1])
+        least_busy_days = [
+            day
+            for day, count in self.count_days.items()
+            if count == min_tuple[1]
+        ]
+        return least_busy_days
