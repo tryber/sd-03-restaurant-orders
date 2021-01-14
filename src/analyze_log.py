d6af878cc1c3ae1e16761f0542457ce0c713df6c
William Igor <williamigorsilva@gmail.com>
2021-01-14 15:21:53 -0300

    Will-restaurant-orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,18 @@
+import csv
+
+
+def get_total_dish(file, person):
+    total_dish = {}
+    for name, order, day in file:
+        if name == person:
+            total_dish[order]
+
+    return len(total_dish)
+
+
 def analyse_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as f:
+        file = csv.reader(f, delimiter=",")
+        result_dish = get_total_dish(file, "maria")
+        with open("data/mkt/campaign.txt", "w"):
+            print(result_dish)
