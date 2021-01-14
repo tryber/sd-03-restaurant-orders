768f08cd4bc3c4296b3aaa8adbd8d419a5c4076c
luldelrey <rpgstrikered@gmail.com>
2021-01-14 13:12:55 -0300

    saving progress, almost done firsts requisites

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,44 +1,104 @@
 import csv
 
 
-def find_most_ordered_item_by_person(order_list, person):
+def client_favorite_order(foods_from_person, person):
     # prato mais pedido por maria
-    return order_list[person]
-
-
-def count_orders_by_food_and_person(order_list, food, person):
-    # count arnaldo hamburgues
-    return order_list
-
-
-def foods_not_ordered_by_person(order_list, person):
-    # comida que joao nao pediu
-    return order_list
-
-
-def days_that_one_person_didnt_go(order_list, person):
-    # dia que joao n foi
-    return order_list
-
-
-def extract_orders_from_file(file):
-    orders_list = {}
-    raw_csv = csv.reader(file, delimiter=",")
-    for person, food, day in raw_csv:
-        if person not in orders_list:
-            orders_list[person] = [food, day]
+    foods_dict = {}
+    most_ordered = foods_from_person[person][0]
+    for food in foods_from_person[person]:
+        if food not in foods_dict:
+            foods_dict[food] = 1
         else:
-            orders_list[person].append(food)
-            orders_list[person].append(day)
-    return orders_list
+            foods_dict[food] += 1
+        if foods_dict[most_ordered] < foods_dict[food]:
+            most_ordered = food
+    return most_ordered
+
+
+def count_orders_by_food_and_person(foods_from_person, query_food, person):
+    # count arnaldo hamburgues
+    orders_by_food = {}
+    for food in foods_from_person[person]:
+        if food not in orders_by_food:
+            orders_by_food[food] = 1
+        else:
+            orders_by_food[food] += 1
+    return orders_by_food[query_food]
+
+
+def never_ordered_by_client(foods_from_person, query_person):
+    # comida que joao nao pediu
+    all_foods = set()
+    for person in foods_from_person:
+        for food in foods_from_person[person]:
+            all_foods.add(food)
+    return all_foods.difference(foods_from_person[query_person])
+
+
+def days_that_one_person_didnt_go(days_from_person, query_person):
+    # dia que joao n foi
+    all_days = set()
+    for person in days_from_person:
+        for day in days_from_person[person]:
+            all_days.add(day)
+    return all_days.difference(days_from_person[query_person])
+
+
+def extract_food_from_file_by_person(data):
+    orders_by_food = {}
+    for person, food, day in data:
+        if person not in orders_by_food:
+            orders_by_food[person] = [food]
+        else:
+            orders_by_food[person].append(food)
+    return orders_by_food
+
+
+def extract_day_from_file_by_person(data):
+    orders_by_day = {}
+    for person, food, day in data:
+        if person not in orders_by_day:
+            orders_by_day[person] = [day]
+        else:
+            orders_by_day[person].append(day)
+    return orders_by_day
+
+
+def get_data(path_to_file):
+    with open(path_to_file) as file:
+        raw_data = csv.reader(file, delimiter=",")
+        data = []
+        for person, food, day in raw_data:
+            data.append([person, food, day])
+    return data
+
+
+def save_data(path_to_file, data_list):
+    with open(path_to_file, "w") as file:
+        for data in data_list:
+            file.write(f"{data}\n")
 
 
 def analyze_log(path_to_file):
-    with open(path_to_file) as file:
-        orders_list = extract_orders_from_file(file)
-        return find_most_ordered_item_by_person(orders_list, 'maria')
+    most_ordered = ""
+    how_many_orders = 0
+    food_not_ordered = set()
+    absent_days = set()
+    data = get_data(path_to_file)
+    foods_from_person = extract_food_from_file_by_person(data)
+    days_from_person = extract_day_from_file_by_person(data)
+    most_ordered = client_favorite_order(foods_from_person, "maria")
+    how_many_orders = count_orders_by_food_and_person(
+        foods_from_person, "hamburguer", "arnaldo"
+    )
+    food_not_ordered = never_ordered_by_client(foods_from_person, "joao")
+    absent_days = days_that_one_person_didnt_go(days_from_person, "joao")
+    save_data(
+        "./data/mkt_campaign.txt",
+        [most_ordered, how_many_orders, food_not_ordered, absent_days],
+    )
 
 
 if __name__ == "__main__":
-    print(analyze_log("./data/orders_1.csv"))
+    analyze_log("./data/orders_1.csv")
     # analyze_log("./data/orders_1.csv")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,82 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def extract_food_from_orders_by_person(self):
+        orders_by_food = {}
+        for person, food, day in self.orders:
+            if person not in orders_by_food:
+                orders_by_food[person] = [food]
+            else:
+                orders_by_food[person].append(food)
+        return orders_by_food
+
+    def extract_day_from_orders_by_person(self):
+        orders_by_day = {}
+        for person, food, day in self.orders:
+            if person not in orders_by_day:
+                orders_by_day[person] = [day]
+            else:
+                orders_by_day[person].append(day)
+        return orders_by_day
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        foods_dict = {}
+        foods_from_person = self.extract_food_from_orders_by_person()
+        most_ordered = foods_from_person[costumer][0]
+        for food in foods_from_person[costumer]:
+            if food not in foods_dict:
+                foods_dict[food] = 1
+            else:
+                foods_dict[food] += 1
+            if foods_dict[most_ordered] < foods_dict[food]:
+                most_ordered = food
+        return most_ordered
+
+    def get_never_ordered_per_costumer(self, costumer):
+        all_foods = set()
+        foods_from_person = self.extract_food_from_orders_by_person()
+        print(foods_from_person)
+        for person in foods_from_person:
+            for food in foods_from_person[person]:
+                all_foods.add(food)
+        return all_foods.difference(foods_from_person[costumer])
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
-
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days_from_person = self.extract_day_from_orders_by_person()
+        return days_from_person
 
     def get_busiest_day(self):
         pass
 
     def get_least_busy_day(self):
         pass
+
+
+if __name__ == "__main__":
+    csv_parsed = [
+        ["maria", "pizza", "terça-feira"],
+        ["maria", "hamburguer", "terça-feira"],
+        ["joao", "hamburguer", "terça-feira"],
+        ["maria", "coxinha", "segunda-feira"],
+        ["arnaldo", "misto-quente", "terça-feira"],
+        ["jose", "hamburguer", "sabado"],
+        ["maria", "hamburguer", "terça-feira"],
+        ["maria", "hamburguer", "terça-feira"],
+        ["joao", "hamburguer", "terça-feira"]
+    ]
+    trackOrders = TrackOrders()
+    for name, food, day in csv_parsed:
+        trackOrders.add_new_order(name, food, day)
+    # print(trackOrders.get_most_ordered_dish_per_costumer("maria"))
+    # print(trackOrders.get_never_ordered_per_costumer("maria"))
+    print(trackOrders.get_days_never_visited_per_costumer("maria"))
