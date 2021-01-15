fcd9627cb46d4dac2b25d4381a5b76787c0530fe
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-15 12:05:09 -0300

    requisito 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,11 +1,11 @@
 import csv
 
 
-def person_most_frequent_meal(persons_meal):
+def person_most_frequent_meal(all_data, person):
     count = {}
-    most_frequent_meal = persons_meal[0]
+    most_frequent_meal = all_data[person]["food"][0]
 
-    for meal in persons_meal:
+    for meal in all_data[person]["food"]:
         if meal not in count:
             count[meal] = 1
         else:
@@ -17,11 +17,11 @@ def person_most_frequent_meal(persons_meal):
     return most_frequent_meal
 
 
-def person_meal_count(person_meal):
+def person_meal_count(all_data, person, order):
     count = 0
 
-    for meal in person_meal:
-        if meal == "hamburguer":
+    for meal in all_data[person]["food"]:
+        if meal == order:
             count += 1
 
     return count
@@ -37,17 +37,6 @@ def person_meal_never_ordered(meals_type, persons_meal):
 
 def person_days_without_going(days_of_the_week, persons_days_going):
     different_persons_going_day = set(persons_days_going)
-    # days_of_the_week = set(
-    #     [
-    #         "segunda-feira",
-    #         "terça-feira",
-    #         "quarta-feira",
-    #         "quinta-feira",
-    #         "sexta-feira",
-    #         "sabado",
-    #         "domingo",
-    #     ]
-    # )
 
     without_going_days = days_of_the_week.difference(
         different_persons_going_day
@@ -64,20 +53,20 @@ def analyze_log(path_to_file):
             "days_of_week": set(),
         }
 
-        for row in readable:
-            data_in_dict["foods_types"].add(row[1])
-            data_in_dict["days_of_week"].add(row[2])
-            if row[0] not in data_in_dict:
-                data_in_dict[row[0]] = {"food": [row[1]], "day": [row[2]]}
+        for customer, order, day in readable:
+            data_in_dict["foods_types"].add(order)
+            data_in_dict["days_of_week"].add(day)
+            if customer not in data_in_dict:
+                data_in_dict[customer] = {"food": [order], "day": [day]}
             else:
-                data_in_dict[row[0]]["food"].append(row[1])
-                data_in_dict[row[0]]["day"].append(row[2])
+                data_in_dict[customer]["food"].append(order)
+                data_in_dict[customer]["day"].append(day)
 
-        most_frequent_meal = person_most_frequent_meal(
-            data_in_dict["maria"]["food"]
+        most_frequent_meal = person_most_frequent_meal(data_in_dict, "maria")
+
+        hamburguer_count = person_meal_count(
+            data_in_dict, "arnaldo", "hamburguer"
         )
-
-        hamburguer_count = person_meal_count(data_in_dict["arnaldo"]["food"])
 
         meals_never_ordered = person_meal_never_ordered(
             data_in_dict["foods_types"], data_in_dict["joao"]["food"]
@@ -88,11 +77,11 @@ def analyze_log(path_to_file):
         )
 
         with open("data/mkt_campaign.txt", mode="w") as file:
-            file.write(f'{str(most_frequent_meal)}\n')
-            file.write(f'{str(hamburguer_count)}\n')
-            file.write(f'{str(meals_never_ordered)}\n')
-            file.write(f'{str(days_without_going)}\n')
+            file.write(f"{str(most_frequent_meal)}\n")
+            file.write(f"{str(hamburguer_count)}\n")
+            file.write(f"{str(meals_never_ordered)}\n")
+            file.write(f"{str(days_without_going)}\n")
 
 
 if __name__ == "__main__":
-    print(analyse_log("./data/orders_1.csv"))
+    print(analyze_log("./data/orders_1.csv"))
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,82 @@
+from src.analyze_log import (
+    person_most_frequent_meal,
+    person_meal_count,
+    person_meal_never_ordered,
+    person_days_without_going,
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self._foods_type = set()
+        self._days_of_week = set()
+        self._orders_per_client = {}
+        self._days_count = {}
+        self._count_orders = 0
+
     def __len__(self):
-        pass
+        return self._count_orders
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self._count_orders += 1
+        self._foods_type.add(order)
+        self._days_of_week.add(day)
+        if costumer not in self._orders_per_client:
+            self._orders_per_client[costumer] = {"food": [order], "day": [day]}
+        else:
+            self._orders_per_client[costumer]["food"].append(order)
+            self._orders_per_client[costumer]["day"].append(day)
+
+        if day not in self._days_count:
+            self._days_count[day] = 1
+        else:
+            self._days_count[day] += 1
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return person_most_frequent_meal(self._orders_per_client, costumer)
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return person_meal_count(self._orders_per_client, costumer, order)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return person_meal_never_ordered(
+            self._foods_type, self._orders_per_client[costumer]["food"]
+        )
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return person_days_without_going(
+            self._days_of_week, self._orders_per_client[costumer]["day"]
+        )
 
     def get_busiest_day(self):
-        pass
+        day_count = 0
+        busiest_day = ""
+
+        for day in self._days_count:
+            if self._days_count[day] > day_count:
+                day_count = self._days_count[day]
+                busiest_day = day
+
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        day_count = 1
+        busiest_day = ""
+
+        for day in self._days_count:
+            if self._days_count[day] <= day_count:
+                day_count = self._days_count[day]
+                busiest_day = day
+
+        return busiest_day
+
+
+if __name__ == "__main__":
+    tacker = TrackOrders()
+    tacker.add_new_order("Jafet", "pizza", "segunda-feira")
+    tacker.add_new_order("Luis", "Camarao", "quarta-feira")
+    tacker.add_new_order("Isabella", "Risito", "terça-feira")
+    tacker.add_new_order("Jafet", "pizza", "segunda-feira")
+    tacker.add_new_order("Jafet", "hamburguer", "segunda-feira")
+
+    print(tacker.get_least_busy_day())
