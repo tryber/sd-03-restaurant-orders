31c8949c317ad387533c4b3d397614fcbba1bc7d
DevT <aguiarthiag@gmail.com>
2021-01-16 11:54:55 -0300

    Removing code runner and manual test generated files

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,62 @@
-def analyse_log(path_to_file):
-    raise NotImplementedError
+import csv
+
+
+def parse_data(path):
+    data = {
+        'menu': set(),
+        'week_days': set()
+    }
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=",", quotechar='"')
+        for costumer, order, day in reader:
+            data["menu"].add(order)
+            data["week_days"].add(day)
+            if costumer not in data:
+                data[costumer] = {"orders": [order], "days": [day]}
+            else:
+                data[costumer]["orders"].append(order)
+                data[costumer]["days"].append(day)
+    return data
+
+
+def most_ordered(costumer, data):
+    orders_dict = dict()
+    orders = data[costumer]['orders']
+    for order in orders:
+        if order not in orders_dict:
+            orders_dict[order] = 1
+        else:
+            orders_dict[order] += 1
+
+    sort_orders = sorted(orders_dict.items(), key=lambda x: x[1], reverse=True)
+
+    return sort_orders[0][0]
+
+
+def specific_order(costumer, data, specific):
+    orders_dict = dict()
+    orders = data[costumer]['orders']
+    for order in orders:
+        if order not in orders_dict:
+            orders_dict[order] = 1
+        else:
+            orders_dict[order] += 1
+
+    return orders_dict[specific]
+
+
+def analyze_log(path_to_file):
+    data = parse_data(path_to_file)
+    menu = data["menu"]
+    wd = data["week_days"]
+
+    maria_most_ordered = most_ordered('maria', data)
+    arnaldos_hamburguer = specific_order('arnaldo', data, 'hamburguer')
+    joao_not_orders = menu.difference(data['joao']['orders'])
+    joao_not_days = wd.difference(data['joao']['days'])
+
+    with open("data/mkt_campaign.txt", mode="w") as file:
+        file.write(f"{str(maria_most_ordered)}\n")
+        file.write(f"{str(arnaldos_hamburguer)}\n")
+        file.write(f"{str(joao_not_orders)}\n")
+        file.write(f"{str(joao_not_days)}\n")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,76 @@
 class TrackOrders:
+    def __init__(self):
+        self.data_dict = dict()
+        self.data_list = list()
+        self.menu = set()
+        self.wd = set()
+
     def __len__(self):
-        pass
+        return len(self.data_list)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.data_list.append([costumer, order, day])
+        self.menu.add(order)
+        self.wd.add(day)
+        if costumer not in self.data_dict:
+            self.data_dict[costumer] = {"orders": [order], "days": [day]}
+        else:
+            self.data_dict[costumer]["orders"].append(order)
+            self.data_dict[costumer]["days"].append(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders_dict = dict()
+        orders = self.data_dict[costumer]['orders']
+        for order in orders:
+            if order not in orders_dict:
+                orders_dict[order] = 1
+            else:
+                orders_dict[order] += 1
+
+        sort_orders = sorted(
+            orders_dict.items(), key=lambda x: x[1], reverse=True)
+
+        return sort_orders[0][0]
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        orders_dict = dict()
+        orders = self.data_dict[costumer]['orders']
+        for order in orders:
+            if order not in orders_dict:
+                orders_dict[order] = 1
+            else:
+                orders_dict[order] += 1
+
+        return orders_dict[order]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.menu.difference(self.data_dict[costumer]['orders'])
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return self.wd.difference(self.data_dict[costumer]['days'])
 
     def get_busiest_day(self):
-        pass
+        days_dict = dict()
+        for order in self.data_list:
+            if order[2] not in days_dict:
+                days_dict[order[2]] = 1
+            else:
+                days_dict[order[2]] += 1
+
+        sort_days = sorted(
+            days_dict.items(), key=lambda x: x[1], reverse=True)
+
+        return sort_days[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days_dict = dict()
+        for order in self.data_list:
+            if order[2] not in days_dict:
+                days_dict[order[2]] = 1
+            else:
+                days_dict[order[2]] += 1
+
+        sort_days = sorted(
+            days_dict.items(), key=lambda x: x[1], reverse=False)
+
+        return sort_days[0][0]
