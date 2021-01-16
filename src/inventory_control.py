44148ff2f1e0fd38203d28fc4f9825e4ef358119
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-16 16:41:39 -0300

    Requisito 3 passando nos testes locais

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,30 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+
+        self.ingredients_to_buy = {}
+
+        self.order_ingredients = list()
+
     def add_new_order(self, costumer, order, day):
-        pass
+        self.order_ingredients = self.ingredients[order]
+        for ingredient in self.order_ingredients:
+            if self.inventory[ingredient] <= 0:
+                return False
+            self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        for ingredient, qty in self.inventory.items():
+            self.ingredients_to_buy[ingredient] = (
+                self.minimum_inventory[ingredient] - self.inventory[ingredient]
+            )
+        return self.ingredients_to_buy
