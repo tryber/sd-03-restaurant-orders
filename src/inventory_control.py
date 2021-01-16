1edaad9b5df09fb04fd70b0608c9b67f911d9f20
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-16 12:50:40 -0300

    100%

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -20,11 +20,12 @@ class InventoryControl:
         self.orders_used_inventory = self.minimum_inventory.copy()
 
     def add_new_order(self, costumer, order, day):
+        if order not in self.get_available_dishes():
+            return False
+
         ingredients = self.ingredients[order]
+
         for ingredient in ingredients:
-            # if ingredient not in self.orders_used_inventory:
-            #     self.orders_used_inventory[ingredient] = 1
-            # else:
             self.orders_used_inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
@@ -37,8 +38,21 @@ class InventoryControl:
             what_to_buy[ingredient] = quantity_to_buy
         return what_to_buy
 
+    def get_available_dishes(self):
+        ingredients = set(self.orders_used_inventory.keys())
+        meals = set(self.ingredients.keys())
+        meal_to_remove = set()
+        for ingredient in ingredients:
+            if self.orders_used_inventory[ingredient] == 0:
+                for meal in meals:
+                    if ingredient in self.ingredients[meal]:
+                        meal_to_remove.add(meal)
+        return meals.difference(meal_to_remove)
+
 
 if __name__ == "__main__":
     inventory = InventoryControl()
-    inventory.add_new_order("jorge", "pizza", "terça-feira")
-    print(inventory.get_quantities_to_buy())
+    for i in range(50):
+        print(inventory.add_new_order("jorge", "pizza", "terça-feira"))
+
+    # print(inventory.get_available_dishes())
