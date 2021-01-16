f2bf12cb30b868c0c8f99d65e9be850fa86bf563
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-16 20:51:19 -0300

    test re2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,45 @@
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        quantity = 0
+
+        for name, recipe, day in self.orders:
+            if name == costumer and recipe == order:
+                quantity += 1
+
+        return quantity
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        orders = set()
+        costumer_orders = set()
+
+        for name, order, day in self.orders:
+            orders.add(order)
+
+            if name == costumer:
+                costumer_orders.add(order)
+
+        return orders.difference(costumer_orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days_on = set()
+        days_with_people = set()
+
+        for name, order, day in self.orders:
+            days_on.add(day)
+
+            if name == costumer:
+                days_with_people.add(day)
+
+        return days_on.difference(days_with_people)
 
     def get_busiest_day(self):
         pass
