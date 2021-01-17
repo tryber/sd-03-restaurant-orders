58a6ce109e3311cc62f1231fb9d0277009e7bc12
clayton pereira <cpereiramt@gmail.com>
2021-01-17 14:47:58 -0400

    salvando alteraçoes no primeiro requisito do projeto

--- tests/test_analyze_log.py
+++ tests/test_analyze_log.py
@@ -1,9 +1,9 @@
-from src.analyze_log import analyse_log
+from src.analyze_log import analyze_log
 import pytest
 
 
 def test_validar_conteudo_do_arquivo_gerado():
-    analyse_log("data/orders_1.csv")
+    analyze_log("data/orders_1.csv")
     FILE_TXT = "data/mkt_campaign.txt"
     with open(FILE_TXT) as f:
         file_txt_file = f.readlines()
@@ -22,10 +22,10 @@ def test_validar_conteudo_do_arquivo_gerado():
 def test_validar_arquivo_inexistente():
     expect_text = "No such file or directory: " "'data/orders_3.csv'"
     with pytest.raises(FileNotFoundError, match=expect_text):
-        assert analyse_log("data/orders_3.csv")
+        assert analyze_log("data/orders_3.csv")
 
 
 def test_validar_arquivo_com_extensao_invalida():
     expect_text = "No such file or directory: " "'data/orders_1.txt'"
     with pytest.raises(FileNotFoundError, match=expect_text):
-        assert analyse_log("data/orders_1.txt")
+        assert analyze_log("data/orders_1.txt")
