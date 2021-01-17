92b727b9f5fec6dd1c9de4667a00db0289ac0281
clayton pereira <cpereiramt@gmail.com>
2021-01-17 13:48:48 -0400

    salvando alteraçoes no projeto

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,11 @@
+import csv
+
+
 def analyse_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as csv_file:
+        file_content = csv.reader(csv_file, delimiter=",")
+        for row in file_content:
+            print(row)
+
+
+# analyse_log("/home/cpereiramt/Documentos/Projetos/python/sd-03-restaurant-orders/data/orders_1.csv")
