c413607b210aac5bad13f0db4006bd656ee03cb0
clayton pereira <cpereiramt@gmail.com>
2021-01-19 19:35:33 -0400

    salvando alteraçoes no requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,12 +5,15 @@ class TrackOrders:
         return len(TrackOrders.orders_list)
 
     def add_new_order(self, costumer, order, day):
-        TrackOrders.orders_list[costumer] = [order, day]        
-        print(TrackOrders.orders_list)
+        TrackOrders.orders_list[costumer] = [costumer, order, day]
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
-        return 0
+        per_costumer_dish = {}
+        if costumer in per_costumer_dish.values():
+            per_costumer_dish[costumer] = [TrackOrders.orders_list[costumer]]
+        else:
+            per_costumer_dish[costumer] = [TrackOrders.orders_list[costumer]]
+        return per_costumer_dish[costumer][0][1]
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
