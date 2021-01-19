754c230003d8523cd20e7665f76e19cf9462686d
luizfernando <luizfernandotic@gmail.com>
2021-01-19 12:23:34 -0300

    prato mais pedido por cliente

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -17,6 +17,26 @@ def best_seller_by_client(client, dict):
     return most_frequent
 
 
+def favorite_dish_by_client(client, selected_dish, dict):
+    count = {}
+    client_list = dict[client]
+    for dish, day in client_list:
+        if dish not in count:
+            count[dish] = 1
+        else:
+            count[dish] += 1
+
+    return count[selected_dish]
+
+
+def get_total_dish(dict):
+    dishes = set()
+    for client, orders in dict.items():
+        for tuples in orders:
+            dishes.add(tuples[0])
+    return dishes
+
+
 def analyse_log(path_to_file):
     raise NotImplementedError
     # iniciando o projeto
