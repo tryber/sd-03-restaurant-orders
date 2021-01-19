6bab3e0ba2dacc13f704ed26b5280c5f9faf50a9
clayton pereira <cpereiramt@gmail.com>
2021-01-18 21:11:56 -0400

    slavando alteraçoes no requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,15 +1,16 @@
 import csv
 
 
-def analyze_csv_lines(file_content):
-    ingredients_list = set({"coxinha", "misto-quente", "pizza"})
-    date_list = set({"sabado", "segunda-feira"})
-    return (more_ask_maria(file_content) + "\n" + "1" + "\n"
-            + f'{ingredients_list}' + "\n" + f'{date_list}'
+def analyze_csv_lines(path_to_file):
+    return (more_ask_maria(path_to_file) + "\n"
+            + f'{how_many_tymes_arnaldo_ask(path_to_file)}' + "\n"
+            + f'{dish_joao_dont_ask(path_to_file)}' + "\n"
+            + f'{dates_joao_do_not_go_restaurant(path_to_file)}'
             )
 
 
-def more_ask_maria(file_content):
+def more_ask_maria(path_to_file):
+    file_content = read_file(path_to_file)
     only_maria_cardapio = []
     for row in file_content:
         if row[0] == "maria":
@@ -18,11 +19,52 @@ def more_ask_maria(file_content):
     return (max(set(only_maria_cardapio), key=only_maria_cardapio.count))
 
 
+def how_many_tymes_arnaldo_ask(path_to_file):
+    only_arnaldo_ask_hamburguer = []
+    file_content = read_file(path_to_file)
+    for row in file_content:
+        if row[0] == "arnaldo" and row[1] == "hamburguer":
+            print("test")
+            only_arnaldo_ask_hamburguer.append(row[1])
+    return len(only_arnaldo_ask_hamburguer)
+
+
+def dish_joao_dont_ask(path_to_file):
+    file_content = read_file(path_to_file)
+    joao_ask = set()
+    receipts = set()
+    for row in file_content:
+        if row[0] == "joao":
+            joao_ask.add(row[1])
+        else:
+            receipts.add(row[1])
+
+    return receipts.difference(joao_ask)
+
+
+def dates_joao_do_not_go_restaurant(path_to_file):
+    file_content = read_file(path_to_file)
+    joao_dates = set()
+    all_dates = set()
+    for row in file_content:
+        if row[0] == "joao":
+            joao_dates.add(row[2])
+        else:
+            all_dates.add(row[2])
+
+    return all_dates.difference(joao_dates)
+
+
+def read_file(path_to_file):
+    file_content = " "
+    csv_file = open(path_to_file)
+    file_content = csv.reader(csv_file, delimiter=",")
+    return file_content
+
+
 def analyze_log(path_to_file):
-    with open(path_to_file) as csv_file:
-        file_content = csv.reader(csv_file, delimiter=",")
-        with open("data/mkt_campaign.txt", "w") as txt_file:
-            print(analyze_csv_lines(file_content), file=txt_file)
+    txt_file = open("data/mkt_campaign.txt", "w")
+    txt_file.write(analyze_csv_lines(path_to_file))
 
 
 # analyze_log("/home/cpereiramt/Documentos/Projetos/python/sd-03-restaurant-orders/data/orders_1.csv")
