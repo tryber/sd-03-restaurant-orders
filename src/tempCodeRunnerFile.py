3d4315c45363a9c9ad2c359935c600e069d9a426
fabemiliano <fab.emiliano@gmail.com>
2021-01-20 14:45:28 -0300

    first commit

--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -14,7 +14,7 @@ def get_most_consumed_dish(result):
                 if mariaDish[arr[1]] > most_consumed_qty:
                     most_consumed_qty = mariaDish[arr[1]]
                     most_consumed_dish = arr[1]
-    return f"{str(most_consumed_dish)}\n"
+    return f"{str(most_consumed_dish)};\n"
 
 
 def consumed_hamburguers_by_arnaldo(result):
@@ -23,7 +23,7 @@ def consumed_hamburguers_by_arnaldo(result):
         if arr[0] == "arnaldo":
             if arr[1] == "hamburguer":
                 hamburher_consumed += 1
-    return f"{str(hamburher_consumed)}\n"
+    return f"{str(hamburher_consumed)};\n"
 
 
 def never_ordered(result):
@@ -37,7 +37,7 @@ def never_ordered(result):
     for i in dishes:
         if i not in joao_orders:
             not_ordered.add(i)
-    return f"{str(not_ordered)}\n"
+    return f"{str(not_ordered)};\n"
 
 
 def never_gone(result):
@@ -54,22 +54,37 @@ def never_gone(result):
     return str(days_not_gone)
 
 
-def analyze_log(filepath):
+def analyse_log(filepath):
     arr = []
-    with open(filepath) as file:
-        result = csv.reader(file, delimiter=";", quotechar='"')
-        for row in result:
-            arr.append(row[0].split(","))
-    most_consumed_dish = get_most_consumed_dish(arr)
-    consumed_hamburguers = consumed_hamburguers_by_arnaldo(arr)
-    never_eaten = never_ordered(arr)
-    days_not_gone = never_gone(arr)
-    file = open("data/mkt_campaign.txt", mode="w")
-    LINES = [
-        most_consumed_dish,
-        consumed_hamburguers,
-        never_eaten,
-        days_not_gone,
-    ]
-    file.writelines(LINES)
-    file.close()
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError("Formato invalido")
+        with open(filepath) as file:
+            result = csv.reader(file, delimiter=";", quotechar='"')
+            for row in result:
+                arr.append(row[0].split(","))
+        most_consumed_dish = get_most_consumed_dish(arr)
+        consumed_hamburguers = consumed_hamburguers_by_arnaldo(arr)
+        never_eaten = never_ordered(arr)
+        days_not_gone = never_gone(arr)
+        print(
+            most_consumed_dish,
+            consumed_hamburguers,
+            never_eaten,
+            days_not_gone,
+        )
+        file = open("data/mkt_campaign.txt", mode="w")
+        LINES = [
+            most_consumed_dish,
+            consumed_hamburguers,
+            never_eaten,
+            days_not_gone,
+        ]
+        file.writelines(LINES)
+        file.close()
+
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
+
+
+analyse_log("data/orders_1.csv")
