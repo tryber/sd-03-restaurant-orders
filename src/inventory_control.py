7939373131503b25c7f106fdcdbf6004a2f60030
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-20 21:18:16 -0300

    Passing locally in bonus requirements

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,42 @@ class InventoryControl:
             'frango': 50,
         }
 
-    def add_new_order(self, costumer, order, day):
-        pass
+        self.orders = []
+
+        self.inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+
+    def add_new_order(self, _customer, order, _day):
+        ingredients = self.ingredients[order]
+        for ingredient in ingredients:
+            if self.inventory[ingredient] > 0:
+                self.inventory[ingredient] -= 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        buy_list = {}
+        ingredient_list = set(self.inventory.keys())
+        original = self.minimum_inventory
+        current = self.inventory
+        for ingredient in ingredient_list:
+            buy_list[ingredient] = original[ingredient] - current[ingredient]
+        return buy_list
+
+    def get_available_dishes(self):
+        ingredient_list = set(self.inventory.keys())
+        restaurant_food = set(self.ingredients.keys())
+        unavailable_food = set()
+        for ingredient in ingredient_list:
+            if self.inventory[ingredient] <= 0:
+                for food in restaurant_food:
+                    if ingredient in self.ingredients[food]:
+                        unavailable_food.add(food)
+        return restaurant_food.difference(unavailable_food)
