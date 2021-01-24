90e25beb283888488bc66aaa53bece05ba4a5938
Gustavo <ggteodor07@gmai.com>
2021-01-24 20:52:41 -0300

    what?

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,25 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.quantities_to_buy = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
     def add_new_order(self, costumer, order, day):
-        pass
+        ingredients = self.ingredients[order]
+        for ingredient in ingredients:
+            if (
+                self.minimum_inventory[ingredient] ==
+                self.minimum_inventory[ingredient]
+            ):
+                return False
+            self.quantities_to_buy[ingredient] += 1
 
     def get_quantities_to_buy(self):
         pass
