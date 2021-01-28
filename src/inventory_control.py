d10088079b593672e2b12ebc35e46974308bd8e8
Hfreitas <hfreitas86@gmail.com>
2021-01-28 18:05:08 -0300

    iniciando requisito 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,24 +1,29 @@
 class InventoryControl:
     def __init__(self):
         self.ingredients = {
-            'hamburguer': ['pao', 'carne', 'queijo'],
-            'pizza': ['massa', 'queijo', 'molho'],
-            'misto-quente': ['pao', 'queijo', 'presunto'],
-            'coxinha': ['massa', 'frango'],
+            "hamburguer": ["pao", "carne", "queijo"],
+            "pizza": ["massa", "queijo", "molho"],
+            "misto-quente": ["pao", "queijo", "presunto"],
+            "coxinha": ["massa", "frango"],
         }
 
         self.minimum_inventory = {
-            'pao': 50,
-            'carne': 50,
-            'queijo': 100,
-            'molho': 50,
-            'presunto': 50,
-            'massa': 50,
-            'frango': 50,
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
         }
 
+        # inicializando lista de compras a partir do inventário mínimo
+        self.quantities_to_buy = dict.fromkeys(self.minimum_inventory, 0)
+
     def add_new_order(self, costumer, order, day):
-        pass
+        order_ingredients = self.ingredients[order]
+        for ingredient in order_ingredients:
+            self.quantities_to_buy[ingredient] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.quantities_to_buy
