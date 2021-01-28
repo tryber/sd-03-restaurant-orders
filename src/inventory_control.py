6db1b6f4b0fec39d396cf5b4c1a62cf0ab2455c4
Hfreitas <hfreitas86@gmail.com>
2021-01-28 18:43:25 -0300

    correção requisito 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -23,6 +23,11 @@ class InventoryControl:
     def add_new_order(self, costumer, order, day):
         order_ingredients = self.ingredients[order]
         for ingredient in order_ingredients:
+            if (
+                self.quantities_to_buy[ingredient]
+                >= self.minimum_inventory[ingredient]
+            ):
+                return False
             self.quantities_to_buy[ingredient] += 1
 
     def get_quantities_to_buy(self):
