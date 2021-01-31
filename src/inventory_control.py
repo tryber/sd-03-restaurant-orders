0b1a62e62a87c1af4614d419d47a69a3c102e140
Mateus Talles <mateustalles@gmail.com>
2021-01-31 18:45:40 -0300

    Requirement 4 halfly done

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -19,9 +19,24 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+
     def add_new_order(self, customer, order, day):
-        self.orders.append((customer, order, day))
+        self.orders.append([(customer, order, day), "Unchecked"])
         self.get_quantities_to_buy()
+        available_dishes = self.get_available_dishes()
+        self.orders[len(self.orders) - 1][1] = "Checked"
+        if not available_dishes:
+            return False
+
 
     def get_quantities_to_buy(self):
         demand = {
@@ -34,9 +49,39 @@ class InventoryControl:
             'frango': 0,
         }
 
-        for _, order, _ in self.orders:
-            ingredients = self.ingredients[order]
-            for item in ingredients:
-                demand[item] += 1
+        order_is_possible = True
 
+        for order_item in self.orders:
+            _, dish, _ = order_item[0]
+            if order_item[1] == "Unchecked":
+                ingredients = self.ingredients[dish]
+                for ingredient in ingredients:
+                    demand[ingredient] += 1
+                    self.inventory[ingredient] -= 1
         return demand
+
+    def get_available_dishes(self):
+        dishes = set([*self.ingredients.keys()])
+        avail_dishes = set([*self.ingredients.keys()])
+
+        order_is_possible = True
+
+        for dish in dishes:
+            ingredients = self.ingredients[dish]
+            for ingredient in ingredients:
+                print(f'Ingrediente: {ingredient}, prato: {dish}, qtd ingrediente: {self.inventory[ingredient]}')
+                if self.inventory[ingredient] < 0:
+                    order_is_possible = False
+                    if dish in avail_dishes:
+                        avail_dishes.remove(dish)
+                elif self.inventory[ingredient] == 0:
+                    if dish in avail_dishes:
+                        avail_dishes.remove(dish)
+
+        if not order_is_possible:
+            return False
+        return avail_dishes
+
+
+
+
