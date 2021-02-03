4b0ca097b2fe418cef192f45af39345bdcbcd023
DiegoRafaelAF <draf0314@gmail.com>
2021-02-02 21:12:51 -0300

    projeto passando localmente

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,48 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.updated_list_of_ingredients = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
+        self.all_dishes = set([
+            'hamburguer',
+            'pizza',
+            'misto-quente',
+            'coxinha',
+        ])
+
+    def remove_dish(self, ingredient):
+        for dish in self.ingredients:
+            ingredients = set(self.ingredients[dish])
+            if ingredient in ingredients:
+                self.all_dishes.discard(dish)
+
     def add_new_order(self, costumer, order, day):
-        pass
+        elements = self.ingredients[order]
+        for ingredient in elements:
+            if (
+                self.updated_list_of_ingredients[ingredient] + 1 <
+                self.minimum_inventory[ingredient]
+            ):
+                self.updated_list_of_ingredients[ingredient] += 1
+            elif (
+                self.updated_list_of_ingredients[ingredient] <
+                self.minimum_inventory[ingredient]
+            ):
+                self.updated_list_of_ingredients[ingredient] += 1
+                self.remove_dish(ingredient)
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        return self.updated_list_of_ingredients
+
+    def get_available_dishes(self):
+        return self.all_dishes
