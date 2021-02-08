7634a17bd7a2793dfa1cd64052eaa188b9e1a737
gabryelryba <gabryel.ryba@gmail.com>
2021-02-08 17:10:24 -0300

    controle de inventorio com remocao de items

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,44 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
+        self.dishes = set(
+            ['hamburguer', 'pizza', 'misto-quente', 'coxinha'])
+
+    def remove_dishes(self, ingredient):
+        for dish in self.ingredients:
+            ingredients = set(self.ingredients[dish])
+            if ingredient in ingredients:
+                self.dishes.discard(dish)
+
     def add_new_order(self, costumer, order, day):
-        pass
+        ingredients = self.ingredients[order]
+        for ingredient in ingredients:
+            if (
+                self.inventory[ingredient] + 1 <
+                self.minimum_inventory[ingredient]
+            ):
+                self.inventory[ingredient] += 1
+            elif (
+                self.inventory[ingredient] + 1 ==
+                self.minimum_inventory[ingredient]
+            ):
+                self.inventory[ingredient] += 1
+                self.remove_dishes(ingredient)
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        return self.inventory
+
+    def get_available_dishes(self):
+        return self.dishes
