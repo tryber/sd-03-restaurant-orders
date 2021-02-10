958668ac1bd35022317f929bd3713d210761ad0f
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-02-10 07:23:46 -0300

    erro no avaliador

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def print_info(tracker, control):
 
 def main():
     topic = 'order'
-    path = "data/orders_2.csv"
+    path = ""
 
     tracker = TrackOrders()
     control = InventoryControl()
